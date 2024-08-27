5a7e3295b7419d894555342d9b96edd7dfda4a4d
Luana-Braicovich <luana.braicovich@est.fi.uncoma.edu.ar>
2024-08-27 19:33:29 -0300

    Actualizacion builder mapa

--- backend/target/generated-sources/annotations/backend/enidades/Mapa.java
+++ backend/target/generated-sources/annotations/backend/enidades/Mapa.java
@@ -1,26 +1,27 @@
 package backend.enidades;
+
 import javax.swing.JPanel;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.Timer;
 
-public class Mapa  extends JPanel implements KeyListener {
-	
+public class Mapa extends JPanel implements KeyListener {
+
 	private final int ancho;
 	private final int alto;
-	private final Celda [][] mapa;
+	private final Celda[][] mapa;
 	private final NivelDificultad dificultad;
 	private final int canFantasmasMax;
 	private final PacMan pacman;
-	private final Fantasma [] fantasmas;
+	private final Fantasma[] fantasmas;
 	private int canFantasmasInicial;
-	private int puntaje;	
+	private int puntaje;
 	private Timer timer;
-	
+
 	public Mapa(int ancho, int alto, Celda[][] mapa, NivelDificultad dificultad, int canFantasmasMax,
 			int canFantasmasInicial, int puntaje, PacMan pacman, Fantasma[] fantasmas) {
-		
+
 		this.ancho = ancho;
 		this.alto = alto;
 		this.mapa = mapa;
@@ -70,20 +71,80 @@ public class Mapa  extends JPanel implements KeyListener {
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-		
+
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		
+
 	}
-	
-	
-	
+
+	public class Builder {
+		private int ancho;
+		private int alto;
+		private Celda[][] mapa;
+		private NivelDificultad dificultad;
+		private int canFantasmasMax;
+		private int canFantasmasInicial;
+		private int puntaje;
+		private PacMan pacman;
+		private Fantasma[] fantasmas;
+
+		public Mapa.Builder setAncho(int ancho) {
+			this.ancho = ancho;
+			return this;
+		}
+
+		public Mapa.Builder setAlto(int alto) {
+			this.alto = alto;
+			return this;
+		}
+
+		public Mapa.Builder setMapa(Celda[][] mapa) {
+			this.mapa = mapa;
+			return this;
+		}
+
+		public Mapa.Builder setDificultad(NivelDificultad dificultad) {
+			this.dificultad = dificultad;
+			return this;
+		}
+
+		public Mapa.Builder setPuntaje(int puntaje) {
+			this.puntaje = puntaje;
+			return this;
+		}
+
+		public Mapa.Builder setPacman(PacMan pacman) {
+			this.pacman = pacman;
+			return this;
+		}
+
+		public Mapa.Builder setFantasmas(Fantasma[] fantasmas) {
+			this.fantasmas = fantasmas;
+			return this;
+		}
+
+		public Mapa.Builder setCantFantasmasMax(int cantFantasmasMax) {
+			this.canFantasmasMax = canFantasmasMax;
+			return this;
+
+		}
+
+		public Mapa.Builder setCantFantasmasInicial(int cantFantasmasInicial) {
+			this.canFantasmasInicial = canFantasmasInicial;
+			return this;
+		}
+
+		public Mapa build() {
+			return new Mapa(ancho, alto, mapa, dificultad, canFantasmasMax, canFantasmasInicial, puntaje, pacman,
+					fantasmas);
+		}
+	}
 
 }
